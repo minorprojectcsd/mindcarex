a0e2cc6ddcc4ff911879c7d762bc0cb0bcfc1f7c
abhinavpadige4 <abhinavpadige06@gmail.com>
2026-02-24 23:43:04 +0530

    adding TURN

--- src/pages/VideoSession.tsx
+++ src/pages/VideoSession.tsx
@@ -59,7 +59,12 @@ export default function VideoSession() {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
           { urls: 'stun:stun1.l.google.com:19302' },
-        ],
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
+        ]
       });
 
       stream.getTracks().forEach((t) => pc.addTrack(t, stream));

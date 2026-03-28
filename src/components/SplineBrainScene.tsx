4b51f2dafbde84fa383bcceca1a5c4f317371436
minior <minorprojectcsd@gmail.com>
2026-03-29 03:10:48 +0530

    Create SplineBrainScene.tsx

---
+++ src/components/SplineBrainScene.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineBrainScene() {
+  return (
+    <div className="w-full h-full">
+      <Spline scene="https://prod.spline.design/YKPcwgy1lZjBMXKb/scene.splinecode" />
+    </div>
+  );
+}
